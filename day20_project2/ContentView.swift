e1e4ece841a83b1b1f772e94bfa74ef4a5ecf770
JohnHeo04 <hjo199456@gmail.com>
2021-04-17 15:23:43 +0900

    Initial Commit

---
+++ day20_project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day20_project2
+//
+//  Created by 허정욱 on 17/04/2021.
+//  Copyright © 2021 JohnHur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
