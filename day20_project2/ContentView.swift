974d4f2c3be16829c050f431f1416ca8417ae6d7
JohnHeo04 <hjo199456@gmail.com>
2021-04-19 00:39:03 +0900

    Day20 : Gradients

--- day20_project2/ContentView.swift
+++ day20_project2/ContentView.swift
@@ -2,29 +2,24 @@
 //  ContentView.swift
 //  day20_project2
 
-//  Day20 : Colors and frames
+//  Day20 : Gradients
 //
 //  Created by  John Hur on 17/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
-
-//  Apple은 기본적으로 상단 바와 하단 바에 '안전 지역'을 설정해 놓음.
-//  하지만, edgesIgnoringSafeArea(.all)을 통해 색을 꽉 채울 수 있음
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // 텍스트에 색 배경을 넣게 됨 .backround(Color.red)
-        
-        // 원하는 만큼 색 채우기
-        // Color.red.frame(width: 200, height: 200)
-        ZStack {
-            Color.yellow.edgesIgnoringSafeArea(.all)
-            Text("Your content")
-        }
+        // 선형 그라데이션
+//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        // 원형 그라데이션
+//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        // Angular 그라데이션
+        AngularGradient(gradient: Gradient(colors: [.red,.yellow,.green,.blue,.purple,.red]), center: .center)
     }
 }
-
+//
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
