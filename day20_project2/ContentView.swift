9e8dc55f5733921a30c7cacebc2477015b271ea6
JohnHeo04 <hjo199456@gmail.com>
2021-04-18 16:04:08 +0900

    day20

--- day20_project2/ContentView.swift
+++ day20_project2/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  day20_project2
 //
-//  Created by 허정욱 on 17/04/2021.
+//  Created by  John Hur on 17/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
 //
 
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+    // VStack은 아래의 Text를 수직으로 출력해 줌. 하지만 여백 x
+    // 이 때, VStack(spacing:20) 를 붙이면 여백이 생김
+        VStack(spacing: 20) {
+            Text("Hello, World!")
+            Text("This is another text view")
+        }
     }
 }
 
