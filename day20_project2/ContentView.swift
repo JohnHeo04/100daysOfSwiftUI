437d6390717e552bcdf7338196d82d5b3f0b4f44
JohnHeo04 <hjo199456@gmail.com>
2021-04-19 01:51:08 +0900

    Day20_project2_part1

--- day20_project2/ContentView.swift
+++ day20_project2/ContentView.swift
@@ -2,21 +2,23 @@
 //  ContentView.swift
 //  day20_project2
 
-//  Day20 : Gradients
+//  Day20 : Showing alert messages
 //
-//  Created by  John Hur on 17/04/2021.
+//  Created by  John Hur on 19/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+
     var body: some View {
-        // 선형 그라데이션
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-        // 원형 그라데이션
-//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-        // Angular 그라데이션
-        AngularGradient(gradient: Gradient(colors: [.red,.yellow,.green,.blue,.purple,.red]), center: .center)
+        Button("Show Alert") {
+            self.showingAlert = true
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("Ok!")))
+        }
     }
 }
 //
