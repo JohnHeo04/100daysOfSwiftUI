ba06af39b93c0cc1001aeb66a2599a724f55193c
JohnHeo04 <hjo199456@gmail.com>
2021-04-18 17:52:53 +0900

    day20

--- day20_project2/ContentView.swift
+++ day20_project2/ContentView.swift
@@ -10,11 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    // VStack은 수직 정렬
     // VStack은 아래의 Text를 수직으로 출력해 줌. 하지만 여백 x
     // 이 때, VStack(spacing:20) 를 붙이면 여백이 생김
+    // VStack(alignment: .leading)을 사용하게 되면 왼쪽 정렬 가능
+        
+    // HStack은 수평 정렬
+        
+    // Spacer()를 쓰게되면, 남아있는 공간으로 밀어버림
         VStack(spacing: 20) {
             Text("Hello, World!")
             Text("This is another text view")
+            Text("Third")
+            Spacer()
         }
     }
 }
