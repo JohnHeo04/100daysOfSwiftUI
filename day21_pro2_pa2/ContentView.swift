a166eb1d617856fc53c8d78757800a06e8e45b23
JohnHeo04 <hjo199456@gmail.com>
2021-04-19 20:48:19 +0900

    day21

--- day21_pro2_pa2/ContentView.swift
+++ day21_pro2_pa2/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  day21_pro2_pa2
-//  Day21 : 
+//  Day21 : Stacking up buttons
 //
 //  Created by Johnhur on 19/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
