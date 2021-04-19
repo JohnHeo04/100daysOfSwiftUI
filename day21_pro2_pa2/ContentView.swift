3d57e9b4524c34e945049c275eb9ed1b28b599b7
JohnHeo04 <hjo199456@gmail.com>
2021-04-19 20:46:58 +0900

    Initial Commit

---
+++ day21_pro2_pa2/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  day21_pro2_pa2
+//  Day21 : 
+//
+//  Created by Johnhur on 19/04/2021.
+//  Copyright © 2021 JohnHur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
