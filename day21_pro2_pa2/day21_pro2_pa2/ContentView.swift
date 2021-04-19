920edc1826a26e6f5aa16afc9ea9aa26bb5fb62b
JohnHeo04 <hjo199456@gmail.com>
2021-04-19 20:39:07 +0900

    day21

---
+++ day21_pro2_pa2/day21_pro2_pa2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day21_pro2_pa2
+//
+//  Created by 허정욱 on 19/04/2021.
+//  Copyright © 2021 JohnHur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
