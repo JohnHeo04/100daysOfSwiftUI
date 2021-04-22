7aac2cce9a13d356dd0c87dfc6742a88a1850f5e
JohnHeo04 <hjo199456@gmail.com>
2021-04-22 17:40:26 +0900

    Day23: Environment modifiers

--- day23_pro3_pa1/ContentView.swift
+++ day23_pro3_pa1/ContentView.swift
@@ -1,32 +1,36 @@
 //
 //  ContentView.swift
 //  day23_pro3_pa1
-//  Day23: Conditional modifiers
+//  Day23: Environment modifiers
 
 //  Created by Johnhur on 22/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
 //
 // Walt Disney once said, "There isno magic in magic, it's all in the details."
 
+// You just need to experiment.
+// 하나의 modifier를 가지고 어디든 넣어봐라,
+// crtl+c,+v 보다 훨 났다.. ㅎㅇㅌ
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedText = false
     
     var body: some View {
-        // 만약 Hello World를 눌렀을때 True면 빨간색 출력, False 파랑 출력
-
-//        Button("Hello World") {
-//            self.useRedText.toggle()
-//        }
-//        .foregroundColor(useRedText ? .red : .blue)
-        // 아래는 다른 View를 반환하기에 조건이 성립되지 않아 실행 X
-        if self.useRedText {
-            return Text("Hello World")
-        } else {
-            return Text("Hello World")
-                .background(Color.red)
+        VStack {
+            Text("Gryffindor")
+            //    .font(.largeTitle)
+                .blur(radius: 5)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+                .blur(radius: 5)
+                .font(.largeTitle)
+            Text("Slytherin")
         }
+//        .font(.title)
+            .blur(radius: 0)
+        // 위에 있는 modifier는 environment modifier로 불려진다.
+
     }
 }
 
