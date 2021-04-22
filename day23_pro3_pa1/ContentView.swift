9ba9f3de7bdb43a795304f18d8a5e11b565c0e7e
JohnHeo04 <hjo199456@gmail.com>
2021-04-22 17:30:42 +0900

    Day23: Conditional modifiers

--- day23_pro3_pa1/ContentView.swift
+++ day23_pro3_pa1/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  day23_pro3_pa1
-//  Day23: Why modifier order matters
+//  Day23: Conditional modifiers
 
 //  Created by Johnhur on 22/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
@@ -11,16 +11,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useRedText = false
+    
     var body: some View {
-        Text("Hello World")
-        .padding()
-            .background(Color.red)
-        .padding()
-            .background(Color.blue)
-        .padding()
-            .background(Color.green)
-        .padding()
-            .background(Color.yellow)
+        // 만약 Hello World를 눌렀을때 True면 빨간색 출력, False 파랑 출력
+
+//        Button("Hello World") {
+//            self.useRedText.toggle()
+//        }
+//        .foregroundColor(useRedText ? .red : .blue)
+        // 아래는 다른 View를 반환하기에 조건이 성립되지 않아 실행 X
+        if self.useRedText {
+            return Text("Hello World")
+        } else {
+            return Text("Hello World")
+                .background(Color.red)
+        }
     }
 }
 
