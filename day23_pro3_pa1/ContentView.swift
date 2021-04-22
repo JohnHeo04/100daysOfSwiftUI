3b971b02ce9c8f940788e99fc9e5740c620a352d
JohnHeo04 <hjo199456@gmail.com>
2021-04-22 17:03:51 +0900

    Day23: Why modifier order matters

--- day23_pro3_pa1/ContentView.swift
+++ day23_pro3_pa1/ContentView.swift
@@ -1,7 +1,8 @@
 //
 //  ContentView.swift
 //  day23_pro3_pa1
-//
+//  Day23: Why modifier order matters
+
 //  Created by Johnhur on 22/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
 //
@@ -11,10 +12,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Text("Hello World")
+        .padding()
             .background(Color.red)
-            .edgesIgnoringSafeArea(.all)
+        .padding()
+            .background(Color.blue)
+        .padding()
+            .background(Color.green)
+        .padding()
+            .background(Color.yellow)
     }
 }
 
