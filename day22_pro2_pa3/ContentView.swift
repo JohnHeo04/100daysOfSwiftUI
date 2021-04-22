6f80d65759028567f4908077c8217b9ba7f34ac4
JohnHeo04 <hjo199456@gmail.com>
2021-04-22 14:28:44 +0900

    day22

--- day22_pro2_pa3/ContentView.swift
+++ day22_pro2_pa3/ContentView.swift
@@ -15,6 +15,8 @@
 // 2.사용자의 현재 점수를 국기 아래에 보여주어라
 // 3.누군가 잘못된 답을 선택하면, 그들에게 실수 메세지를 알려줘라 - 예를 들면 "Wrong! That's the flag of France,"
 
+// 맞으면 +1 틀리면 +0 누적 함수 만들기
+
 import SwiftUI
 
 struct ContentView: View {
@@ -41,7 +43,7 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .fontWeight(.black)
                 }
-                 
+                 // 0,1,2
                 ForEach(0 ..< 3) { number in Button(action: {
                     self.flagTapped(number)
                 }) { Image(self.countries[number])
@@ -60,7 +62,6 @@ struct ContentView: View {
                    .font(.largeTitle)
                }
                Spacer()
-
             }
         }
         // 아래의 userScore를 넣어 알림창에서 마지막 몇점에서 끝났는지 알려줌
@@ -73,16 +74,27 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[correctAnswer])"
+            userScore -= 1
         }
-        
         showingScore = true
     }
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    // if correct +=1, wrong +=0 or -=1
+    // 스코어 함수 생성
+    // 생략... 아래의 함수를 통해 점수가 올라가고 내려가는 함수를 만들려 했으나
+    // flagTapped의 함수 중
+    // 아래 userScore += 1 이라는 방법을 써서 간단하게 해결할 수 있었음
+//    func scores() {
+//        if scoreTitle
+//        userScore
+//
+//    }
     
 }
 
