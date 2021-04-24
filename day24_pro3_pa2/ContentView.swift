2f3c4ba4c44ed1d6f8c37cc5348a6773dd0f4744
JohnHeo04 <hjo199456@gmail.com>
2021-04-24 22:27:50 +0900

    day24 : Challenge 2

--- day24_pro3_pa2/ContentView.swift
+++ day24_pro3_pa2/ContentView.swift
@@ -1,16 +1,65 @@
 //
 //  ContentView.swift
 //  day24_pro3_pa2
+//  Day24 : Challgenge Day
 //
-//  Created by 허정욱 on 23/04/2021.
+//  Created by JohnHur on 23/04/2021.
 //  Copyright © 2021 JohnHur. All rights reserved.
 //
+//  Challenge.2
+//  Go back to project 1 "WeSplit" and use a conditional modifier to change the total amount text view to red if the user selects a 0% tip.
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
