4a489aa969b6b616c04991819ff4852b827dd8af
JohnHeo04 <hjo199456@gmail.com>
2021-04-28 21:34:48 +0900

    day26 : Training a model with Create ML(Machine Learning)

--- day26_/ContentView.swift
+++ day26_/ContentView.swift
@@ -1,11 +1,17 @@
 //
 //  ContentView.swift
-//  day26 : Working with dates
+//  day26 : Training a model with Create ML(Machine Learning)
 //
 //  Created by JohnHur on 2021/04/28.
 //
-//  86,400 seconds = 1day
+//  Paul Hudson님이 가지고 있는 CSV파일을 이용해 기계학습을 시킨다.
+//  여기서 쓰이는 샘플 피쳐들은 이 프로젝트를 위해 쓰인다.
 
+//  만약 사람들이 잠을 얼마나 자야하는지 알고 싶다면, 커피 몇잔을 마시는지 학습시켜 답을 예측할 수 있다.
+//  'actualSleep'을 타겟변수로 놓고 다른 피쳐(wake, estimateSleep, and coffee)들을 선택 변수로 놓는다.
+//  CSV파일을 업로드하고 아래 보면 여러 알고리즘이 있는데 지금 학습자는 자동선택으로 데이터를 학습시켰다.
+
+//  재밌다.....
 
 import SwiftUI
 
@@ -13,22 +19,7 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     
     var body: some View {
-//        단락이 넘어 갈 수록 애플에서는 개발자에게 쉬운방법을 제공해준다는걸 볼 수 있음.
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? Date()
-        
-//        let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
-//        let hour = components.hour ?? 0
-//        let minute = components.minute ?? 0
-        
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
-        
-        
-        return DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
     }
 }
