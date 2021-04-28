1ba95e497b3525fe860cd6e35f6997ff77b5159d
JohnHeo04 <hjo199456@gmail.com>
2021-04-28 13:07:59 +0900

    Initial Commit

---
+++ day26_/day26_App.swift
@@ -0,0 +1,17 @@
+//
+//  day26_App.swift
+//  day26_
+//
+//  Created by 허정욱 on 2021/04/28.
+//
+
+import SwiftUI
+
+@main
+struct day26_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
