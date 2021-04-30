9d9d869b2f425fe6aa2dabbf13ada66fdf085bcd
JohnHeo04 <hjo199456@gmail.com>
2021-04-30 09:28:12 +0900

    day27 : Cleaning up the user interface

--- day27_pro4_pa2/ContentView.swift
+++ day27_pro4_pa2/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  day27_pro4_pa2
-//  day27 : Building a basic layout
+//  day27 : Cleaning up the user interface
 //
 //  Created by John Hur on 2021/04/29.
 //
@@ -12,26 +12,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) { Text("\(sleepAmount, specifier: "%g") hours")
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+                    
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) { Text("\(sleepAmount, specifier: "%g") hours")
+                    }
                 }
                 
+                VStack(alignment: .leading, spacing: 0) {
                 Text("Daily coffee intake")
                     .font(.headline)
                 
@@ -40,23 +50,55 @@ struct ContentView: View {
                     Text("1 cup")
                 } else {
                     Text("\(coffeeAmount) cups")
+                        }
                     }
                 }
             }
             .navigationBarTitle("BetterRest")
-            // 오른쪽 상단 위에 trailing을 통해 'Calculate'라는 텍스트를 놓을 수 있다.
-            // 만약 여러 버튼을 놓고 싶다면 HStack을 써서 가능
-            // *action: calculateBedtimes를 통해 함수를 불러와 계산을 하게 됨.
             .navigationBarItems(trailing:
                                     Button(action: calculateBedtimes) {
                                         Text("Calculate")
                                     }
             )
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
-        //  나중에 하게되는 몇시에 자러가야하는지 알려주는 함수
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
         func calculateBedtimes() {
+            let model = SleepCalculator()
+
+            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
             
+            do {
+                let prediction = try
+                    model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+                
+                let sleepTime = wakeUp - prediction.actualSleep
+                // more code here
+                let formatter = DateFormatter()
+                formatter.timeStyle = .short
+                
+                alertMessage = formatter.string(from: sleepTime)
+                alertTitle = "Your ideal bedtime is..."
+                
+            } catch {
+                // something went wrong!
+                alertTitle = "Error"
+                alertMessage = "Sorry, there was a problem calculating your bedtime."
+            }
+            
+            showingAlert = true
     }
 }
 
