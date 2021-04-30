2985b1eefe2ffa484d33a510857d06c8256d5232
JohnHeo04 <hjo199456@gmail.com>
2021-04-30 17:29:40 +0900

    Initial Commit

---
+++ day28_pro4_pa3/day28_pro4_pa3App.swift
@@ -0,0 +1,17 @@
+//
+//  day28_pro4_pa3App.swift
+//  day28_pro4_pa3
+//
+//  Created by John Hur on 2021/04/30.
+//
+
+import SwiftUI
+
+@main
+struct day28_pro4_pa3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
