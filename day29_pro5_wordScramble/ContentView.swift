f5a3fd104797d492fa01d149a4ea0b6f9a0f3135
JohnHeo04 <hjo199456@gmail.com>
2021-05-04 13:56:50 +0900

    Initial Commit

---
+++ day29_pro5_wordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day29_pro5_wordScramble
+//
+//  Created by John Hur on 2021/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day29_pro5_wordScramble/day29_pro5_wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  day29_pro5_wordScrambleApp.swift
+//  day29_pro5_wordScramble
+//
+//  Created by John Hur on 2021/05/04.
+//
+
+import SwiftUI
+
+@main
+struct day29_pro5_wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
