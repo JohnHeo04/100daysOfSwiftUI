3a49885812615152cfd1d7758dab3e52a2d33c30
JohnHeo04 <hjo199456@gmail.com>
2021-05-06 06:55:35 +0900

    Day29: Working with strings

--- day29_pro5_wordScramble/ContentView.swift
+++ day29_pro5_wordScramble/ContentView.swift
@@ -1,25 +1,42 @@
 //
 //  ContentView.swift
 //  day29_pro5_wordScramble
-//  Day29: Loading resources from your app bundle
+//  Day29: Working with strings
 
 //  Created by John Hur on 2021/05/04.
 //
-//  URL이 웹주소 저장하는것보다 조금 더 강력하다.
-//  URL은 파일의 위치까지 저장할 수 있다.
+//  iOS는 문자열을 다룰 수 있는 강력한 API를 제공함
+//  문자열을 배열로 분리하기도하고, 공백을 제거하고, 스펠링을 체크하기도 함
 
-//
+//  components(separatedBy: )는 싱글 문자열로 분해 해줌
+//  "a b c" --> ["a", "b", "c"]
+
+//  trimmingCharacters(in:)는 모든 기능을 하나의 메소드에서 제공함
+//  공간, 텝, 라인 바꾸기 등
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            // we found the file in our bundle
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string
-            }
-        }
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
+        
+//        let input = """
+//            a
+//            b
+//            c
+//            """
+//        let letters = input.components(separatedBy: "\n")
+//        let letter = letters.randomElement()
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+//        let input = "a b c"
+//        let letters = input.components(separatedBy: "")
         
        return Text("Hello World!")
     }
