f562d688aca9754b39d6f4c9e36e8c08b0faa332
JohnHeo04 <hjo199456@gmail.com>
2021-05-06 06:24:16 +0900

    Day29: Loading resources from your app bundle

--- day29_pro5_wordScramble/ContentView.swift
+++ day29_pro5_wordScramble/ContentView.swift
@@ -5,12 +5,23 @@
 
 //  Created by John Hur on 2021/05/04.
 //
+//  URL이 웹주소 저장하는것보다 조금 더 강력하다.
+//  URL은 파일의 위치까지 저장할 수 있다.
+
+//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Text("Hello World!")
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            // we found the file in our bundle
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a string
+            }
+        }
+        
+       return Text("Hello World!")
     }
 }
 
