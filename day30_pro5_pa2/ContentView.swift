29864d5c1866d402223d639d7faa57f4e9a8bcf3
JohnHeo04 <hjo199456@gmail.com>
2021-05-06 15:22:13 +0900

    Initial Commit

---
+++ day30_pro5_pa2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day30_pro5_pa2
+//
+//  Created by John Hur on 2021/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day30_pro5_pa2/day30_pro5_pa2App.swift
@@ -0,0 +1,17 @@
+//
+//  day30_pro5_pa2App.swift
+//  day30_pro5_pa2
+//
+//  Created by John Hur on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct day30_pro5_pa2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
