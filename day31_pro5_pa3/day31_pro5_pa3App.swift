7756a0149dbd910cb7016738f8927147cb3f14ee
JohnHeo04 <hjo199456@gmail.com>
2021-05-08 22:00:55 +0900

    Initial Commit

---
+++ day31_pro5_pa3/day31_pro5_pa3App.swift
@@ -0,0 +1,17 @@
+//
+//  day31_pro5_pa3App.swift
+//  day31_pro5_pa3
+//
+//  Created by John Hur on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct day31_pro5_pa3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
