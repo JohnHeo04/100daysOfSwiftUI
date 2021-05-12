fbd3825015a8d52709dfb07f0db5cb9c224d4d44
JohnHeo04 <hjo199456@gmail.com>
2021-05-12 23:12:04 +0900

    Day32 : Customizing animations in SwiftUI

--- day32_pro6_pa1/ContentView.swift
+++ day32_pro6_pa1/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  day32_pro6_pa1
-//  Day32 : Creating implicit animations
+//  Day32 : Customizing animations in SwiftUI
 //
 //  Created by John Hur on 2021/05/11.
 //
@@ -15,19 +15,33 @@ struct ContentView: View {
         Button("Tap Me") {
             // 버튼을 'Tap'하게 되면 크기가 1씩 증가
             // 변수 값 animationAmount에 탭할때 마다 값 +1
-            self.animationAmount += 1
+//            self.animationAmount += 1
             
         }
         .padding(50)
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        // 아래의 값 1.0은 100%랑 같음, 만약 CGFloat이 5면 500%로 5배 커진걸 볼 수 있음
-        .scaleEffect(animationAmount)
-        // animationAmount - 1 은 Blur효과를 0부터 시작하게 해줌
-        // 다음 3포인트 6포인트 9포인트씩 증가함
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default)
+        // 버튼 위에 빨간 원형 선을 만든다.
+        // opacity 2-1 = 1로 설정
+        // 두번째 animationAmount에 +=1 이 되면 0이되어 투명해짐
+        .overlay(
+            Circle()
+                    .stroke(Color.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(Double(2 - animationAmount))
+                    .animation(
+                        Animation.easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false)
+            //                .repeatCount(3, autoreverses: true)
+                )
+            )
+        .onAppear {
+            self.animationAmount = 2
+        }
+//        .scaleEffect(animationAmount)
+        
+
     }
 }
 
