091b7fab753f7b7a4c4c646221355d57991558fe
JohnHeo04 <hjo199456@gmail.com>
2021-05-13 11:06:50 +0900

    Day33 : Controlling the animation stack

--- day33_pro6_pa2/ContentView.swift
+++ day33_pro6_pa2/ContentView.swift
@@ -1,16 +1,31 @@
 //
 //  ContentView.swift
 //  day33_pro6_pa2
+//  Day33 : Controlling the animation stack
 //
 //  Created by John Hur on 2021/05/13.
 //
+//  animation() modifier는 여러 개 쓸 수 도 있음
 
 import SwiftUI
 
 struct ContentView: View {
+    // false라면 enabled ? 00 : 01 중 01을 먼저 call함
+    @State private var enabled = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            // toogle은 on/off - true/false를 통해 control
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 :  0))
+        // 아래의 modifier를 통해 animate할때 자연스럽게 해줌
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        
     }
 }
 
