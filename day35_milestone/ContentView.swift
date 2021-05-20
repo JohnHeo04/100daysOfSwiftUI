1203f6cc30fcebfac229a44728f3407f0d97e3ce
JohnHeo04 <hjo199456@gmail.com>
2021-05-20 18:24:46 +0900

    Initial Commit

---
+++ day35_milestone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day35_milestone
+//
+//  Created by John Hur on 2021/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day35_milestone/day35_milestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  day35_milestoneApp.swift
+//  day35_milestone
+//
+//  Created by John Hur on 2021/05/20.
+//
+
+import SwiftUI
+
+@main
+struct day35_milestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
