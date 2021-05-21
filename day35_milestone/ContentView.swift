208601acecbb2603e968fe15afb54e70d75f169e
JohnHeo04 <hjo199456@gmail.com>
2021-05-21 14:59:48 +0900

    ...

--- day35_milestone/ContentView.swift
+++ day35_milestone/ContentView.swift
@@ -5,13 +5,24 @@
 //
 //  Created by John Hur on 2021/05/20.
 //
+//  Milestone : Challenge
+//  My Goal is to build an "edutainment" app for kids to help them practice multiplication tables - "What is 7 x 8?" and so on.
+
+//  Breaking it down :
+//  * The player needs to select which multiplication tables they want to practice. This could be pressing buttons, or it could be an "Up to..." stepper, going from 1 to 12.
+//  * The player should be able to select how many questions they want to be asked: 5, 10, 20, or "ALL".
+//  * You should randomly generate as many questions as they asked for, whitin the difficulty range they asked for. For the "all" case you should generate all possible combinations.
 
 import SwiftUI
 
 struct ContentView: View {
+    // 여기 필요한 기본 변수명 추가 하는 곳
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 이곳이 View추가, H,V,Z -Stack
+        VStack {
+            print("Hello World")
+        }
     }
 }
 
